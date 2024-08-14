90321f9eb8f3ec045f7fbeb7d0c078d4dbea4f27
Efron723 <a161026521@gmail.com>
2024-08-15 02:29:02 +0800

    update port

--- bin/www.js
+++ bin/www.js
@@ -15,7 +15,7 @@ import 'dotenv/config.js'
  * Get port from environment and store in Express.
  */
 
-var port = normalizePort(process.env.PORT || '3005')
+var port = normalizePort(process.env.PORT || '10000')
 app.set('port', port)
 
 /**
