b8ec8c08f8847689db45ca9e421212f072a44e01
Efron723 <a161026521@gmail.com>
2024-08-15 20:45:40 +0800

    update www.js

--- bin/www.js
+++ bin/www.js
@@ -1,43 +1,35 @@
-/**
- * Module dependencies.
- */
-
 import app from '../app.js'
 import debugLib from 'debug'
 import http from 'http'
 const debug = debugLib('node-express-es6:server')
 import { exit } from 'node:process'
 
-// 導入dotenv 使用 .env 檔案中的設定值 process.env
+// 導入 dotenv 使用 .env 檔案中的設定值 process.env
 import 'dotenv/config.js'
 
 /**
  * Get port from environment and store in Express.
  */
-
-var port = normalizePort(process.env.PORT || '3005')
+const port = normalizePort(process.env.PORT || '10000') // 使用預設值 10000 作為本地開發的端口
 app.set('port', port)
 
 /**
  * Create HTTP server.
  */
-
-var server = http.createServer(app)
+const server = http.createServer(app)
 
 /**
  * Listen on provided port, on all network interfaces.
  */
-
-server.listen(port)
+server.listen(port, '0.0.0.0') // 確保綁定到所有網絡接口
 server.on('error', onError)
 server.on('listening', onListening)
 
 /**
  * Normalize a port into a number, string, or false.
  */
-
 function normalizePort(val) {
-  var port = parseInt(val, 10)
+  const port = parseInt(val, 10)
 
   if (isNaN(port)) {
     // named pipe
@@ -55,13 +47,12 @@ function normalizePort(val) {
 /**
  * Event listener for HTTP server "error" event.
  */
-
 function onError(error) {
   if (error.syscall !== 'listen') {
     throw error
   }
 
-  var bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port
+  const bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port
 
   // handle specific listen errors with friendly messages
   switch (error.code) {
@@ -81,9 +72,8 @@ function onError(error) {
 /**
  * Event listener for HTTP server "listening" event.
  */
-
 function onListening() {
-  var addr = server.address()
-  var bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port
+  const addr = server.address()
+  const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port
   debug('Listening on ' + bind)
 }
