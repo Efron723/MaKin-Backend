2b5b340bda905d86766b1379962874b5b30ff803
efron <a161026521@gmail.com>
2025-05-22 17:27:50 +0800

    update sequelize

--- configs/db.js
+++ configs/db.js
@@ -23,23 +23,6 @@ const sequelize = new Sequelize(
     },
   }
 )
-// for postgresql test
-// const sequelize = new Sequelize(
-//   process.env.PG_DB_DATABASE,
-//   process.env.PG_DB_USERNAME,
-//   process.env.PG_DB_PASSWORD,
-//   {
-//     host: process.env.PG_DB_HOST,
-//     port: process.env.PG_DB_PORT,
-//     dialect: 'postgres',
-//     logging: false,
-//     define: {
-//       freezeTableName: true,
-//       charset: 'utf8',
-//       collate: 'utf8_general_ci',
-//     },
-//   }
-// )
 
 // 啟動時測試連線
 sequelize
@@ -56,14 +39,6 @@ sequelize
 
 // 載入models中的各檔案
 await applyModels(sequelize)
-
-// 同步化模型與資料庫結構
-// 注意，這只會更改資料庫中的表，而不會更改JS端的模型
-// 需要更動資料表的範例資料(seeds)，請使用`npm run seed`指令
-// sync 的值有以下三種
-// { alter: true } 檢查資料庫中資料表的當前狀態(它有哪些列,它們的資料類型等),然後在表中進行必要的更改，使其與模型匹配.
-// { force: true } 將建立資料表,如果表已經存在,則將其首先刪除
-// {} 如果表不存在,則建立該表(如果已經存在,則不執行任何操作)
 await sequelize.sync({})
 
 console.log(
