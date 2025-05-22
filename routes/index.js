a4cef348588839e0f92312c27831835f4a38a3be
efron <a161026521@gmail.com>
2025-05-22 23:02:55 +0800

    update homepage

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 import express from 'express'
+import { ready } from '../configs/db.js' // 你之前 export 的連線 Promise
+
 const router = express.Router()
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' })
+router.get('/', async (req, res, next) => {
+  let dbStatus = '資料庫尚未連線'
+
+  try {
+    await ready
+    dbStatus = '✅ 資料庫已成功連線'
+  } catch (error) {
+    dbStatus = '❌ 資料庫連線失敗'
+  }
+
+  res.render('index', {
+    title: '首頁',
+    dbStatus, // 傳給模板
+  })
 })
 
 export default router
